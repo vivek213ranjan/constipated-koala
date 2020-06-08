94df13be56616945eedcbe042fd6c18fa60f740a
Nireon <siemvdtweel@gmail.com>
2020-06-08 16:41:11 +0200

    Add documentation payconiq_webhook.md and fixed some minor bugs

--- db/migrate/20200527123642_ideal_transactions_to_payment.rb
+++ db/migrate/20200527123642_ideal_transactions_to_payment.rb
@@ -1,5 +1,5 @@
 class IdealTransactionsToPayment < ActiveRecord::Migration[6.0]
-  def change
+  def up
     rename_table :ideal_transactions, :payments
     rename_column :payments, :redirect_uri, :ideal_redirect_uri
 
@@ -9,5 +9,17 @@ class IdealTransactionsToPayment < ActiveRecord::Migration[6.0]
     add_column :payments, :payment_type, :integer, default: 0
 
     change_column :checkout_transactions, :payment_method, :string, limit: 32
+  end
+
+  def down
+
+    remove_column :payments,:payment_type
+    remove_column :payments,:transaction_type
+
+    add_column :payments, :transaction_type,:string
+    rename_column :payments, :ideal_redirect_uri, :redirect_uri
+
+    rename_table :payments, :ideal_transactions
+    change_column :checkout_transactions, :payment_method, :string, limit:7
   end
 end
