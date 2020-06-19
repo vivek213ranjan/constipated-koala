a9d3847719db1f43944e0232becb1e7d4757b7b5
Nireon <siemvdtweel@gmail.com>
2020-06-19 12:05:00 +0200

    Fixed migrations + removed some unused files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_123642) do
+ActiveRecord::Schema.define(version: 2020_06_19_120150) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", null: false
