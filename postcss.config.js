0f74fe8fb53d54e9ad58a4a97c1e3cdd4f2f7b3d
Rick van Hoef <rickvhoef@gmail.com>
2020-09-02 10:57:29 +0200

    use purgecss to minify package size in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+let environment = {
   plugins: [
     require('tailwindcss'),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -11,3 +12,17 @@ module.exports = {
     })
   ]
 }
+
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.haml',
+        './app/assets/javascript/**/*.js',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+module.exports = environment;
